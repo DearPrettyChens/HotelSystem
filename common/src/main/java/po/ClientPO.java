1c4b98f0c6167c901e9d52a0dfed6faf1dd98195
cccyf <731767381@qq.com>
2016-11-17 14:01:43 +0800

    clients添加类型

--- common/src/main/java/po/ClientPO.java
+++ common/src/main/java/po/ClientPO.java
@@ -2,34 +2,45 @@ package po;
 
 import java.io.Serializable;
 
+import util.UserType;
+
 /**
  * 获取当前用户的用户名和密码
+ * 
  * @author CLL
  * @version 1.0
  */
-public class ClientPO implements Serializable{
+public class ClientPO implements Serializable {
 	private String userName;
 	private String password;
 	private int userID;
-	
-	public ClientPO(String userName,String password,int userID){
-		this.userName=userName;
-		this.password=password;
-		this.userID=userID;
+	private UserType type;
+
+	public ClientPO(String userName, String password, int userID) {
+		this.userName = userName;
+		this.password = password;
+		this.userID = userID;
 	}
-	
+
+	public ClientPO(String userName, String password, int userID, UserType type) {
+		this.userName = userName;
+		this.password = password;
+		this.userID = userID;
+		this.type = type;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -40,5 +51,13 @@ public class ClientPO implements Serializable{
 
 	public void setUserID(int userID) {
 		this.userID = userID;
+	}
+
+	public UserType getType() {
+		return type;
+	}
+
+	public void setType(UserType type) {
+		this.type = type;
 	}
 }
