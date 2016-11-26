5d29946fd926cef778221981554c30de68448854
Lili0820 <1179028267@qq.com>
2016-11-26 10:49:42 +0800

    解决searchhotelbl接口更改导致的报错

--- client/src/main/java/presentation/ui/searchhotelui/distributecontroller/SearchhotelDistributionController.java
+++ client/src/main/java/presentation/ui/searchhotelui/distributecontroller/SearchhotelDistributionController.java
@@ -7,6 +7,7 @@ import businesslogic.blservice.searchhotelblservice.SearchHotelBLService;
 import util.HotelSortType;
 import util.OrderState;
 import vo.searchhotelvo.HotelListVO;
+import vo.searchhotelvo.HotelSearchInfoVO;
 
 /**
  * searchHotel界面调用逻辑层的控制类
@@ -26,16 +27,6 @@ public class SearchhotelDistributionController {
 		return controller;
 	}
 	
-	/**
-	 * 获取酒店列表
-	 * @param 无
-	 * @return  ArrayList<HotelListVO> ，将酒店列表返回给界面
-	 * @throws 未定
-	 *
-	 */
-	public ArrayList<HotelListVO> getHotelList(){
-		return searchHotelBLService.getHotelList();
-	}
 	
 	/**
 	 * 获取特定类型的酒店列表
@@ -44,18 +35,8 @@ public class SearchhotelDistributionController {
 	 * @throws 未定
 	 *
 	 */
-	public ArrayList<HotelListVO> getSortedHotelList(HotelSortType type){
-		return searchHotelBLService.getSortedHotelList(type);
+	public ArrayList<HotelListVO> getSortedHotelList(HotelSearchInfoVO hotelSearchInfoVO){
+		return searchHotelBLService.getSortedHotelList(hotelSearchInfoVO);
 	}
 	
-	/**
-	 * 获取特定类型的预定过的酒店列表
-	 * @param type HotelSortType型,orderState OrderState型，传递列表类型 
-	 * @return  ArrayList<HotelListVO> ，将特定类型的预定过的酒店列表返回给界面
-	 * @throws 未定
-	 *
-	 */
-	public ArrayList<HotelListVO> getBookedHotelList(HotelSortType type,OrderState orderState,String ID){
-		return searchHotelBLService.getBookedHotelList(type, orderState, ID);
-	}
 }
