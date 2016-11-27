f1217369d6f711cc53c40d7ed18916b314b46343
Green-Cherry <2423964104@qq.com>
2016-11-27 08:43:01 +0800

    为了测试RMI

--- server/src/main/java/init/ServerRunner.java
+++ server/src/main/java/init/ServerRunner.java
@@ -7,8 +7,8 @@ public class ServerRunner {
 	public static void main(String[] args) {
 		RMIHelper.connect();
         System.out.println("服务器端RMI连接OK");
-//        RMIHelper.release();
-//        System.out.println("服务器端口释放成功");
+        RMIHelper.release();
+        System.out.println("服务器端口释放成功");
 	}
 
 }
