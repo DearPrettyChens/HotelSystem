c1b43fc5bf40ec26974ca1729281f534d32d71d3
Lili0820 <1179028267@qq.com>
2016-11-30 20:45:09 +0800

    同上一条

---
+++ client/src/main/java/presentation/ui/personnelui/view/Personlistinfo_JPanel.java
@@ -0,0 +1,30 @@
+package presentation.ui.personnelui.view;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class Personlistinfo_JPanel extends JPanel{
+	
+	
+	public Personlistinfo_JPanel(){
+		
+	
+		
+		
+		this.setSize(700,200);
+		this.setLayout(null);
+	//	this.setBackground(new Color(204,204,204));
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
