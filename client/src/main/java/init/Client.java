cd5b374cd329c0fe846957346726e84b2d086a2c
Green-Cherry <2423964104@qq.com>
2016-12-11 14:37:19 +0800

    client

--- client/src/main/java/init/Client.java
+++ client/src/main/java/init/Client.java
@@ -2,21 +2,19 @@ package init;
 
 import java.rmi.RemoteException;
 
+import javax.swing.JFrame;
+
 import dao.availableroomdao.AvailableRoomDao;
 import po.AvailableRoomInfoPO;
+import presentation.ui.loginui.view.login_JFrame;
 import util.BedType;
 
 public class Client {
 	public static void main(String[] args) {
-	     RMIHelper.init();
-	     System.out.println("客户端连接成功");
-	     AvailableRoomDao availableRoomDao=RMIHelper.getAvailableRoomDao();
-	     try {
-			System.out.println(availableRoomDao.addAvailableRoomInfo(new AvailableRoomInfoPO(13, "hhhh", BedType.BIGBED, 444, 333, 5,3)));
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	    JFrame frame=new login_JFrame();
+	    RMIHelper.init();
+	     
+	     
 	}
 
 
