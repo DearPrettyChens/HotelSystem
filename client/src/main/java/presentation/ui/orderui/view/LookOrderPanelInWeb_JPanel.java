58cdc7d7cb6f20af25f0e7df504523fca9054633
cccyf <731767381@qq.com>
2016-12-14 16:25:26 +0800

    push一下

--- client/src/main/java/presentation/ui/orderui/view/LookOrderPanelInWeb_JPanel.java
+++ client/src/main/java/presentation/ui/orderui/view/LookOrderPanelInWeb_JPanel.java
@@ -58,8 +58,16 @@ public class LookOrderPanelInWeb_JPanel extends JPanel {
 	 * 界面初始化获得异常订单信息
 	 */
 	public void initOrderList(OrderState state) {
-		TypeInfoVO typeInfoVO=new TypeInfoVO(UserType.WebMarketMan, state, null );
-		ArrayList<OrderListVO> orderListVOs=orderDistributionController.getOrderList(typeInfoVO);
+		ArrayList<OrderListVO> orderListVOs =new ArrayList<OrderListVO>() ;
+		if(state==OrderState.EXECUTED){
+			TypeInfoVO typeInfoVO=new TypeInfoVO(UserType.WebMarketMan, OrderState.HASREMARKED, null );
+			orderListVOs.addAll(orderDistributionController.getOrderList(typeInfoVO));
+			typeInfoVO.setOrderState(OrderState.NOTREMARKED);
+			orderListVOs.addAll(orderDistributionController.getOrderList(typeInfoVO));
+		}else{
+		    TypeInfoVO typeInfoVO=new TypeInfoVO(UserType.WebMarketMan, state, null );
+		    orderListVOs=orderDistributionController.getOrderList(typeInfoVO);
+		}
 	    changeScrollPane(orderListVOs);
 	}
     
@@ -74,12 +82,12 @@ public class LookOrderPanelInWeb_JPanel extends JPanel {
 		searchResultPanel.repaint();
 //		scrollPane.repaint();
 		searchResultPanel.setBackground(Color.WHITE);
-		 orderlistinfo_JPanels=new ArrayList<Orderlistinfo_JPanel>();
+		orderlistinfo_JPanels=new ArrayList<Orderlistinfo_JPanel>();
 		for(OrderListVO orderListVO:orderListVOs){
 	    	Orderlistinfo_JPanel orderlistinfo_JPanel=new Orderlistinfo_JPanel(orderListVO);
 	    	orderlistinfo_JPanels.add(orderlistinfo_JPanel);
 	    }
-		System.out.println(orderlistinfo_JPanels.size());
+//		System.out.println(orderlistinfo_JPanels.size());
 	    for(int i=0;i<orderlistinfo_JPanels.size();i++){
 	    	Orderlistinfo_JPanel orderlistinfo_JPanel=orderlistinfo_JPanels.get(i);
 	    	orderlistinfo_JPanel.setBounds(100, 10+120*i, 800, 100);
